239276dcf11da12173124bdeb9e91bb145674caa
Sagar Patel <17.sagar.patel@gmail.com>
2014-12-02 15:56:46 +0900

    Implemented BCI data loading and created a streaming data emulator for easier transition to live data down the line

---
+++ SagarFolder/BCIVisualExperiment1/BCIExp1UnityFolder/BCIVisualExp1/Assets/Scripts/BCIDataDirector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class BCIDataDirector : MonoBehaviour 
+{
+
+	BCIStreamingDataEmulator streamingDataEmulator;
+
+	void Awake()
+	{
+		streamingDataEmulator = FindObjectOfType<BCIStreamingDataEmulator>();
+	}
+
+
+	void Update()
+	{
+
+		Debug.Log("Latest data: " + streamingDataEmulator.GetDataPointsArray()[0] );
+	}
+
+}
