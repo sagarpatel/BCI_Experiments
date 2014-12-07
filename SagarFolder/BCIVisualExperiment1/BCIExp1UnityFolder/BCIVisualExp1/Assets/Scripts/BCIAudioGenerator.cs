8a0259c5042731a01627c60271be7ae331b7118a
Sagar Patel <17.sagar.patel@gmail.com>
2014-12-07 14:11:22 +0900

    Basic procedural audio generation based on the BCI data stream

---
+++ SagarFolder/BCIVisualExperiment1/BCIExp1UnityFolder/BCIVisualExp1/Assets/Scripts/BCIAudioGenerator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BCIAudioGenerator : MonoBehaviour 
+{
+	BCIDataDirector bciDataDirector;
+
+	public float gain = 0.1f;
+
+	void Awake()
+	{
+
+		bciDataDirector = FindObjectOfType<BCIDataDirector>();
+
+	}
+
+	void OnAudioFilterRead(float[] data, int channels)
+	{
+
+
+		float[] bciData = bciDataDirector.currentDataArray;
+		int bciDataLength = bciData.Length;
+		int audioDataLength = data.Length/channels;
+
+
+		// assuming that bciDataLength is always smaller than data length
+		int sampleRatio = audioDataLength/bciDataLength ;
+		for(int i  = 0; i < data.Length ; i  = i + channels)
+		{
+			int theoreticalBCILocation = i / sampleRatio;
+
+			if(theoreticalBCILocation >= bciData.Length)
+				break;
+
+
+
+			data[i] = gain * bciData[theoreticalBCILocation];
+
+			// copy data to both channels if exist
+			if(channels == 2)
+			{
+				data[i +1] = data[i];
+			}
+		}
+
+
+
+	}
+
+
+}
