c1af3085e8852287c24f9b7a64c46cd2b2c6d36f
Sagar Patel <17.sagar.patel@gmail.com>
2014-12-28 13:17:53 +0530

    Got basic OSC connection to muse working, some egg data being transferred by default. Need to select desired channels next

--- SagarFolder/BCIVisualExperiment1/BCIExp1UnityFolder/BCIVisualExp1/Assets/Scripts/BCILiveDataManager.cs
+++ SagarFolder/BCIVisualExperiment1/BCIExp1UnityFolder/BCIVisualExp1/Assets/Scripts/BCILiveDataManager.cs
@@ -1,12 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityOSC;
+using System.Collections.Generic;
 
 public class BCILiveDataManager : MonoBehaviour 
 {
 	void Start()
 	{
+		OSCHandler.Instance.Init();
 
+	}
+
+	void Update()
+	{
+
+		OSCHandler.Instance.UpdateLogs();
+		Dictionary<string, ServerLog> serversDict = new Dictionary<string, ServerLog>();
+		serversDict = OSCHandler.Instance.Servers;
+
+		Debug.Log("Looping through servers");
+		ServerLog tempServerLog = new ServerLog();
+		foreach( string serverKey in serversDict.Keys)
+		{
+			serversDict.TryGetValue(serverKey, out tempServerLog);
+			Debug.Log(tempServerLog.log[0]);
+		}
 
 	}
 
